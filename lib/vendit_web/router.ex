5dc66d9653dccd12cc5e9265dfae6f0a3a670a1a
Howard Beard-Marlowe <howard.beard-marlowe@dazzle.rocks>
2024-01-15 11:36:37 +0200

    [AUTH] Initial authentication system

--- lib/vendit_web/router.ex
+++ lib/vendit_web/router.ex
@@ -1,6 +1,8 @@
 defmodule VenditWeb.Router do
   use VenditWeb, :router
 
+  import VenditWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule VenditWeb.Router do
     plug :put_root_layout, html: {VenditWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -24,4 +27,42 @@ defmodule VenditWeb.Router do
   # scope "/api", VenditWeb do
   #   pipe_through :api
   # end
+
+  ## Authentication routes
+
+  scope "/", VenditWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{VenditWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", VenditWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{VenditWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", VenditWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{VenditWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
+    end
+  end
 end
